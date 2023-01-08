20d09dd56b9a7c7bbc6219f695b675f1082f36b4
nabeeljb <115611865+nabeeljb@users.noreply.github.com>
2023-01-08 14:11:01 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 sudo yum install httpd -y
 sudo systemctl start httpd
-sudo wget -P https://www.free-css.com/assets/files/free-css-templates/download/page284/pet-shop.zip
+sudo wget -P /var/www/html https://www.free-css.com/assets/files/free-css-templates/download/page284/pet-shop.zip
 sudo unzip -o /var/www/html/pet-shop.zip -d /var/www/html
 sudo cp -rf /var/www/html/pet-shop-website-template/* /var/www/html/
 sudo systemctl restart httpd.service
